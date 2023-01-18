039ef3ef8822e6ec2d5d2311197cc04a4e9cc4e5
Giovani Junior <giovaniocjr+github@gmail.com>
2023-01-18 08:51:50 -0300

    Listando strutcs no template

--- web/main.go
+++ web/main.go
@@ -10,9 +10,10 @@ import (
 )
 
 type Post struct {
-	Id    int
-	Title string
-	Body  string
+	Id      int
+	Title   string
+	Body    string
+	Created string
 }
 
 var db, err = sql.Open("mysql", "root:abc123456@tcp(localhost:3306)/cursogoweb?charset=utf8")
@@ -26,31 +27,22 @@ func main() {
 
 	for rows.Next() {
 		post := Post{}
-		rows.Scan(&post.Id, &post.Title, &post.Body)
+		rows.Scan(&post.Id, &post.Title, &post.Body, &post.Created)
 		items = append(items, post)
 	}
 
-	//stmt, err := db.Prepare("INSERT INTO posts (title, body) VALUES (?,?);")
-	//checkErr(err)
-
-	//_, err = stmt.Exec("Post 2", "Content 2")
-	//checkErr(err)
 	db.Close()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		post := Post{Id: 1, Title: "Unamed Post", Body: "No content"}
-
-		if title := r.FormValue("title"); title != "" {
-			post.Title = title
-		}
-
 		t := template.Must(template.ParseFiles("templates/index.html"))
-		if err := t.ExecuteTemplate(w, "index.html", post); err != nil {
+		if err := t.ExecuteTemplate(w, "index.html", items); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
 	fmt.Println((http.ListenAndServe(":8080", nil)))
+
 }
 
 func checkErr(err error) {
@@ -58,3 +50,11 @@ func checkErr(err error) {
 		panic(err)
 	}
 }
+
+//func add() {
+//stmt, err := db.Prepare("INSERT INTO posts (title, body) VALUES (?,?);")
+//checkErr(err)
+
+//_, err = stmt.Exec("Post 2", "Content 2")
+//checkErr(err)
+//}
